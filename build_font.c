da3c3be5fbdea84e213030ce016a0327baaf3526
Gianluca Mazzini <gianluca@mazzini.org>
2023-08-07 16:29:39 +0200

    Update build_font.c

--- build_font.c
+++ build_font.c
@@ -18,7 +18,7 @@ int main (int argc,char **argv){
   fp=fopen("my.ff","rb");
   fread(F,16,1,fp);
   if(memcmp(F,"farbfeld",8)!=0)exit(-1);
-  x=F[8]<<24|F[9]<<16|F[10]<<8|F[11];
+  x=((long)F[8])<<24|((long)F[9])<<16|((long)F[10])<<8|((long)F[11]);
   y=F[12]<<24|F[13]<<16|F[14]<<8|F[15];
   y--;
   v=x*y;

c351ab1624e9aebb4e410a4e9dd7b94fd8acef66
Gianluca Mazzini <gianluca@mazzini.org>
2023-08-07 06:18:15 +0200

    Update build_font.c

--- build_font.c
+++ build_font.c
@@ -3,26 +3,20 @@
 #include "string.h"
 
 int main (){
-  unsigned char F[16],A[8],*H;
+  char buf[100],*p;
   FILE *fp;
-  unsigned long x,y,v;
-  int n,t;
+  int x,y;
 
-  fp=fopen("my.ff","rb");
-  fread(F,16,1,fp);
-  if(memcmp(F,"farbfeld",8)!=0)exit(-1);
-  x=F[8]<<24|F[9]<<16|F[10]<<8|F[11];
-  y=F[12]<<24|F[13]<<16|F[14]<<8|F[15];
-  t=x*y;
-  H=malloc(t);
-  for(n=0;n<t;n++){
-    fread(A,8,1,fp);
-    v=A[0]<<16|A[2]<<8|A[4];
-    printf("%ld ",v);
-  }
+  fp=fopen("my.pbm","rb");
+  fgets(buf,100,fp);
+  if(memcmp(buf,"P1",2)!=0)exit(-1);
+  fgets(buf,100,fp);
+  p=strtok(" ",buf);
+  *p='\0';
+  x=atoi(buf);
+  y=atoi(p);
   fclose(fp);
   
-
-  printf("%ld %ld\n",x,y);
+  printf("%d %d\n",x,y);
 
 }

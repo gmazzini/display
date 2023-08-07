fc8ef72b514f3c6fe1fb592acd402223717380cd
Gianluca Mazzini <gianluca@mazzini.org>
2023-08-07 06:50:32 +0200

    Update build_font.c

--- build_font.c
+++ build_font.c
@@ -17,12 +17,12 @@ int main (){
   for(n=0;n<v;){
     fread(F,8,1,fp);
     a=F[0]<<16|F[2]<<8|F[4];
-    if(a==0)printf("0");else print("1");
+    if(a==0)printf("0");else printf("1");
     n++;
     if(n%x==0)printf("\n");
   }
   fclose(fp);
   
-  printf("%d %d\n",x,y);
+  printf("%ld %ld\n",x,y);
 
 }

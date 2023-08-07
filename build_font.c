9150beb3b014ba6140f25bb037bf8cf5f6db6e2b
Gianluca Mazzini <gianluca@mazzini.org>
2023-08-07 16:19:47 +0200

    Update build_font.c

--- build_font.c
+++ build_font.c
@@ -20,6 +20,7 @@ int main (int argc,char **argv){
   if(memcmp(F,"farbfeld",8)!=0)exit(-1);
   x=F[8]<<24|F[9]<<16|F[10]<<8|F[11];
   y=F[12]<<24|F[13]<<16|F[14]<<8|F[15];
+  y--;
   v=x*y;
   p=malloc(v);
   for(n=0;n<v;n++){

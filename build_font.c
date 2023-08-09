47ce3df2947111da4d98c5b8b39caea60057acfb
Gianluca Mazzini <gianluca@mazzini.org>
2023-08-09 03:39:23 +0200

    Update build_font.c

--- build_font.c
+++ build_font.c
@@ -8,16 +8,14 @@ int main (int argc,char **argv){
   FILE *fp;
   unsigned long x,y,v,n,rx,ry,w,r,ty,from_y,to_y,wx;
 
+  // ty from_y to_y wx name.ff
   ty=atol(argv[1]);
   from_y=atol(argv[2]);
   to_y=atol(argv[3]);
   wx=atol(argv[4]);
-  
-  printf("/* font %ld\n",ty);
-  for(n=33;n<127;n++)printf("%c",(char)n);
-  printf("\n*/\n");
-  
-  fp=fopen("my.ff","rb");
+
+  // read ff file with !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
+  fp=fopen(argv[5],"rb");
   fread(F,16,1,fp);
   if(memcmp(F,"farbfeld",8)!=0)exit(-1);
   x=F[8]<<24|F[9]<<16|F[10]<<8|F[11];
@@ -35,31 +33,30 @@ int main (int argc,char **argv){
   l=to_y-from_y+1;
   printf("unsigned int font_%ld[97][%d]={\n",ty,l);
 
+  // header
   printf("  { /* header */\n");
   printf("    %ld, /* ty */\n",ty);
   printf("    %d, /* y */\n",l);
   printf("    %ld, /* wx */\n",wx);
-  for(n=3;n<l;n++){
-    if(n==l-1)printf("    0\n");
+  for(n=3;n<=l;n++){
+    if(n==l)printf("    0\n"); 
     else printf("    0,\n");
   }
   printf("  },\n");
 
+  // space
   printf("  { /* c=space */\n");
+  l=(wx==0)?1:wx;
+  printf("    %d",l);
   for(ry=from_y-1;ry<to_y;ry++){
     printf("    0b");
-    l=(wx==0)?1:wx;
-    for(n=0;n<12;n++)printf("0");
-    ll=l<<4;
-    for(n=12;n<16;n++){
-      printf("%d",(ll&0x80)?1:0);
-      ll<<=1;
-    }
+    for(n=0;n<16;n++)printf("0");
     if(ry==y-1)printf("\n");
     else printf(",\n");
   }
   printf("  },\n");
 
+  // looking for vertical blanks
   for(rx=0;rx<x;rx++){
     w=0;
     for(ry=from_y-1;ry<to_y;ry++)if(p[rx+ry*x]!=0)w++;
@@ -78,7 +75,8 @@ int main (int argc,char **argv){
       kk[rx]=w;
     }
   }
-  /* double apex */
+  
+  // double apex processing
   l=0;
   for(rx=0;rx<x;rx++){
     if(kk[rx]>0&&l==1)break;
@@ -90,22 +88,19 @@ int main (int argc,char **argv){
   for(rx++;rx<x;rx++)if(kk[rx]==0)break;
   kk[l]=rx-l;
   for(n=l+1;n<rx;n++)kk[n]=-1;
-    
+
+  // character processing
   r=33;
   for(rx=0;rx<x;rx++){
     if( wx>0 && rx%(wx+1)==0 && r<127 || wx==0 && kk[rx]>0 && r<127 ){
       if(wx==0)l=kk[rx];
       else l=wx;
       printf("  { /* c=%ld,%c */\n",r,(char )r);
+      printf("    %d",l);
       for(ry=from_y-1;ry<to_y;ry++){
         printf("    0b");
         for(n=0;n<l;n++)printf("%d",p[rx+n+ry*x]);
-        for(n=l;n<12;n++)printf("0");
-        ll=l<<4;
-        for(n=12;n<16;n++){
-          printf("%d",(ll&0x80)?1:0);
-          ll<<=1;
-        }
+        for(n=l;n<16;n++)printf("0");
         if(ry==to_y-1)printf("\n");
         else printf(",\n");
       }

3d806ecdaacfb6a9233cba23666d9ac6d9c52f03
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-02 10:53:18 +0200

    Update matrix.php

--- matrix.php
+++ matrix.php
@@ -106,7 +106,7 @@ if($ser=="0029"){
   $name=sprintf("tmp/aldini/%03d.ff",$vf+1);
   shell_exec("tmp/convert3 $name 10 $bin");
 }
-else if($ser=="0019"){
+else if($ser=="0100"){
   $query=oci_parse($conn,"update mysession set c1=c1+1 where id='$ip'");
   oci_execute($query);
   $query=oci_parse($conn,"select c1 from mysession where id='$ip'");

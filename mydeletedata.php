c58280cf3fef9d7a7199bee92845a0adb19dbf26
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-05 18:01:44 +0200

    Update mydeletedata.php

--- mydeletedata.php
+++ mydeletedata.php
@@ -3,4 +3,20 @@
 mydelete($conn,"idistat");
 mydelete($conn,"istatente");
 
+mydelete($conn,"man");
+mydelete($conn,"pal");
+mydelete($conn,"scuole");
+mydelete($conn,"areeaai");
+mydelete($conn,"uiftth");
+mydelete($conn,"aziendeaai");
+mydelete($conn,"apwifi");
+
+mydelete($conn,"attivifse");
+mydelete($conn,"accessifse");
+mydelete($conn,"scaricatifse");
+mydelete($conn,"attivazionilepidaid");
+mydelete($conn,"accessilepidaid");
+mydelete($conn,"sportellilepidaid");
+
+
 ?>

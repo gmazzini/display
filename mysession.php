be0f911f1eeb74dc707999cf0df0a50367221a0c
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-10 05:37:04 +0200

    Update and rename tool_sessions.php to mysession.php

---
+++ mysession.php
@@ -0,0 +1,36 @@
+<?php
+
+include "data.php";
+$conn=oci_connect($p1,$p2,$p3);
+
+echo "<pre>";
+$query=oci_parse($conn,"select id,ser,iter,c1,c2 from mysession order by id");
+oci_execute($query);
+for(;;){
+  $row=oci_fetch_row($query);
+  if($row==null)break;
+
+  $istat="";
+  $aux=explode(".",$row[0]);
+  if($aux[0]=="10"){
+    $id=$aux[1]*256+$aux[2];
+    $query1=oci_parse($conn,"select istat from idistat where '$id'>=idstart and '$id'<=idend");
+    oci_execute($query1);
+    $row1=oci_fetch_row($query1);
+    $istat=$row1[0];
+    oci_free_statement($query);
+  }
+  
+  $query1=oci_parse($conn,"select sovra from idistat where istat='$istat'");
+  oci_execute($query1);
+  $row1=oci_fetch_row($query1);
+  @$sovra=$row1[0];
+  oci_free_statement($query1);
+  
+  echo "$row[0],$row[1],$row[2],$row[3],$row[4],$istat,$sovra\n";
+}
+oci_free_statement($query);
+
+oci_close($conn);
+
+?>

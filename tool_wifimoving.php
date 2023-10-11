8bf964c44adcd194d07e4f47d5c9d0c31ec02eb1
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-11 05:07:08 +0200

    Update tool_wifimoving.php

--- tool_wifimoving.php
+++ tool_wifimoving.php
@@ -3,7 +3,7 @@
 include "data.php";
 $conn=oci_connect($p1,$p2,$p3);
 
-$query=oci_parse($conn,"select count(distinct istat),id from dhcpwifi where id in (select distinct id from dhcpwifi)");
+$query=oci_parse($conn,"select count(distinct istat),id from dhcpwifi group by id");
 oci_execute($query);
 for(;;){
   $row=oci_fetch_row($query);

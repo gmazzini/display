70ef31ca8e626d1535a68cfb2004ab6f52876380
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-18 06:07:12 +0200

    Update mysession.php

--- mysession.php
+++ mysession.php
@@ -4,7 +4,7 @@ include "data.php";
 $conn=oci_connect($p1,$p2,$p3);
 
 echo "<pre>";
-$query=oci_parse($conn,"select id,ser,iter,c1,c2 from mysession order by id");
+$query=oci_parse($conn,"select id,ser,iter,c1,c2 from mysession order by ser");
 oci_execute($query);
 for(;;){
   $row=oci_fetch_row($query);
@@ -21,11 +21,11 @@ for(;;){
   }
   else if($aux[0]=="WEB")$istat=$aux[3];
   else $istat="37006";
-  $query=oci_parse($conn,"select sovra from idistat where istat='$istat'");
-  oci_execute($query);
-  $row=oci_fetch_row($query);
-  @$sovra=$row[0];
-  oci_free_statement($query);
+  $query1=oci_parse($conn,"select sovra from idistat where istat='$istat'");
+  oci_execute($query1);
+  $row1=oci_fetch_row($query1);
+  @$sovra=$row1[0];
+  oci_free_statement($query1);
   
   echo "$row[0],$row[1],$row[2],$row[3],$row[4],$istat,$sovra\n";
 }

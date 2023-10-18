6f3fe177be3f47d149af347889166cee9b1a1bdb
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-18 06:05:54 +0200

    Update mysession.php

--- mysession.php
+++ mysession.php
@@ -4,23 +4,28 @@ include "data.php";
 $conn=oci_connect($p1,$p2,$p3);
 
 echo "<pre>";
-$query=oci_parse($conn,"select id,ser,iter,c1,c2 from mysession order by id");
+$query=oci_parse($conn,"select id,ser,iter,c1,c2 from mysession order by ser");
 oci_execute($query);
 for(;;){
   $row=oci_fetch_row($query);
   if($row==null)break;
 
-  $istat="";
   $aux=explode(".",$row[0]);
   if($aux[0]=="10"){
     $id=$aux[1]*256+$aux[2];
-    $query1=oci_parse($conn,"select istat,sovra from idistat where '$id'>=idstart and '$id'<=idend");
+    $query1=oci_parse($conn,"select istat from idistat where '$id'>=idstart and '$id'<=idend");
     oci_execute($query1);
     $row1=oci_fetch_row($query1);
     $istat=$row1[0];
-    @$sovra=$row1[1];
     oci_free_statement($query1);
   }
+  else if($aux[0]=="WEB")$istat=$aux[3];
+  else $istat="37006";
+  $query=oci_parse($conn,"select sovra from idistat where istat='$istat'");
+  oci_execute($query);
+  $row=oci_fetch_row($query);
+  @$sovra=$row[0];
+  oci_free_statement($query);
   
   echo "$row[0],$row[1],$row[2],$row[3],$row[4],$istat,$sovra\n";
 }

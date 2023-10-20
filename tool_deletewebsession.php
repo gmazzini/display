8992ff2275ae90cd8fbb6e507ba472b49c89cd71
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-20 06:12:49 +0200

    Update tool_deletewebsession.php

--- tool_deletewebsession.php
+++ tool_deletewebsession.php
@@ -3,7 +3,7 @@
 include "data.php";
 $conn=oci_connect($p1,$p2,$p3);
 
-$query=oci_parse($conn,"delete from mysession where ip like 'WEB.%'");
+$query=oci_parse($conn,"delete from mysession where id like 'WEB.%'");
 oci_execute($query);
 oci_free_statement($query);
 

5c33272dd304e5d79cb07c7f9d1d4cb55bd3aa61
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-25 06:15:35 +0200

    Update matrix.php

--- matrix.php
+++ matrix.php
@@ -95,6 +95,35 @@ function show2($base,$tot,$ip,$bin,$time,$conn){
   shell_exec("tmp/convert3 $name $time $bin");
 }
 
+function show3($base,$tot,$ip,$bin,$time,$conn){
+  $query=oci_parse($conn,"select c1,c2 from mysession where id='$ip'");
+  oci_execute($query);
+  $row=oci_fetch_row($query);
+  $c1=$row[0];
+  $c2=$row[1];
+  oci_free_statement($query);
+  $query=oci_parse($conn,"select m,num from rnd1 where p=$tot");
+  oci_execute($query);
+  $row=oci_fetch_row($query);
+  $m=$row[0];
+  $num=$row[1];
+  oci_free_statement($query);
+  $id=floor($c1 / $m) % $num;
+  query=oci_parse($conn,"select a,c from rnd2 where m=$m and id=$id");
+  oci_execute($query);
+  $row=oci_fetch_row($query);
+  $a=$row[0];
+  $c=$row[1];
+  oci_free_statement($query);
+  $next=($a * $c2 + $c) % $m;
+  $query=oci_parse($conn,"update mysession set c1=c1+1,c2=$next where id='$ip'");
+  oci_execute($query);
+  oci_free_statement($query);
+  $vf=$base + ($next<$tot) ? $next : rand(0,$tot-1);  
+  $name=sprintf("tmp/image/%04d.ff",$vf);
+  shell_exec("tmp/convert3 $name $time $bin");
+}
+
 $query=oci_parse($conn,"select count(*) from mysession where id='$ip'");
 oci_execute($query);
 $row=oci_fetch_row($query);
@@ -165,6 +194,7 @@ switch($screen){
   case "9001": show2(1,76,$ip,$bin,$time,$conn); break;
   case "9002": show2(1001,28,$ip,$bin,$time,$conn); break;
   case "9003": show2(2001,258,$ip,$bin,$time,$conn); break;
+  case "9103": show3(2001,258,$ip,$bin,$time,$conn); break;
 
   case "7001":
   $aux=mysplit($ente,12);

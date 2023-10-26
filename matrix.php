7c7b2f2517c77690b67ba6b59a5bce7e955a1e6d
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-26 08:43:11 +0200

    Update matrix.php

--- matrix.php
+++ matrix.php
@@ -193,8 +193,8 @@ switch($screen){
 
   case "9001": show2(1,76,$ip,$bin,$time,$conn); break;
   case "9002": show2(1001,28,$ip,$bin,$time,$conn); break;
-  case "9003": show2(2001,258,$ip,$bin,$time,$conn); break;
-  case "9103": show3(2001,258,$ip,$bin,$time,$conn); break;
+  case "9003": show2(2001,580,$ip,$bin,$time,$conn); break;
+  case "9103": show3(2001,580,$ip,$bin,$time,$conn); break;
 
   case "7001":
   $aux=mysplit($ente,12);

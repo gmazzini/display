5b78b79ad8d9bdc0507207d85f6003600e785c9d
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-31 06:31:48 +0100

    Update clock.c

--- clock.c
+++ clock.c
@@ -3,16 +3,16 @@
 #include "math.h"
 #include "time.h"
 
-void point1(unsigned char *F,int x,int y){
+void point1(unsigned char *F,int x,int y,int dr,int dg,int db){
   unsigned char *a;
   a=F+16+(x+y*64)*8;
-  *a=255;
-  *(a+2)=255;
-  *(a+4)=255;
+  *a=dr;
+  *(a+2)=dg;
+  *(a+4)=db;
   return;
 }
 
-void line1(unsigned char *F,int x1,int y1,int x2,int y2){
+void line1(unsigned char *F,int x1,int y1,int x2,int y2,int dr,int dg,int db){
   double a,b,len,dx,x,y;
   if(x1==x2){
     if(y1>y2){a=y1; y1=y2; y2=a;}
@@ -51,8 +51,8 @@ int main(int argc,char **argv){
   if(hh>11)hh-=12;
   hr=(90-30*hh)*2*M_PI/360;
   mr=(90-6*mm)*2*M_PI/360;
-  line1(F,31,31,(int)floor(31+20*cos(hr)),(int)floor(31+20+sin(hr)));
-  line1(F,31,31,(int)floor(31+25*cos(mr)),(int)floor(31+25+sin(mr)));
+  line1(F,31,31,(int)floor(31+20*cos(hr)),(int)floor(31+20+sin(hr)),255,0,0);
+  line1(F,31,31,(int)floor(31+25*cos(mr)),(int)floor(31+25+sin(mr)),0,255,0);
   
    // write ff file
   fp=fopen(argv[2],"wb");

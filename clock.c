b5951577a995d99fbdc4cc67f0ca7e1fbcf2b763
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-31 07:17:15 +0100

    Update clock.c

--- clock.c
+++ clock.c
@@ -58,12 +58,9 @@ int main(int argc,char **argv){
   if(hh>11)hh-=12;
   hr=(90-30*hh)*2*M_PI/360;
   mr=(90-6*mm)*2*M_PI/360;
-
-  printf("%f %d %d %f %d %d\n",hr,31+(int)(20*cos(hr)),63-31-(int)(20*sin(hr)),mr,31+(int)(30*cos(mr)),63-31-(int)(30*sin(mr)));
-
-  
-  line1(F,31,31,31+(int)(20*cos(hr)),63-31-(int)(20*sin(hr)),100,0,0);
-  line1(F,31,31,31+(int)(30*cos(mr)),63-31-(int)(30*sin(mr)),0,100,0);
+ 
+  line1(F,31,31,31+(int)(18*cos(hr)),63-31-(int)(18*sin(hr)),120,0,0);
+  line1(F,31,31,31+(int)(25*cos(mr)),63-31-(int)(25*sin(mr)),0,120,0);
   
    // write ff file
   fp=fopen(argv[2],"wb");

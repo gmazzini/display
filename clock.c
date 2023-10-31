772ac65269a247a891badafe907a53d4430bbd06
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-31 18:47:04 +0100

    Update clock.c

--- clock.c
+++ clock.c
@@ -18,7 +18,7 @@ void point1(unsigned char *F,double x,double y,unsigned char *ooo){
   return;
 }
 
-void line1(unsigned char *F,double x1,double y1,double x2,double y2,int v,unsigned char *ooo){
+void line1(unsigned char *F,unsigned char *ooo,double x1,double y1,double x2,double y2,int v){
   double a,b,len,dx,x,y;
   if(x1>x2){a=x1; x1=x2; x2=a; a=y1; y1=y2; y2=a;}
   a=(y1-y2)/(x1-x2);
@@ -38,26 +38,24 @@ void line1(unsigned char *F,double x1,double y1,double x2,double y2,int v,unsign
   return;
 }
 
-char *read1(unsigned char *F,double x,double y){
+void *read1(unsigned char *F,unsigned char *rr,double x,double y){
   unsigned char *a;
-  static unsigned char rr[3];
   if(x>63)x=63;
   if(x<0)x=0;
   if(y>63)y=63;
   if(y<0)y=0;
   a=F+16+((int)x+((int)y)*64)*8;
-  *rr=*a;
-  *(rr+1)=*(a+2);
-  *(rr+2)=*(a+4);
-  return rr;
+  rr[0]=a[0];
+  rr[1]=a[2];
+  rr[2]=a[4];
+  return;
 }
 
-char *ave1(unsigned char *F,double x1,double y1,double x2,double y2,int v){
+void *ave1(unsigned char *F,unsigned char *ooo,double x1,double y1,double x2,double y2,int v){
   double a,b,len,dx,x,y,rrr[3];
-  unsigned char *rr;
-  static unsigned char ooo[3]; ;
+  unsigned char rr[3];
   int cc;
-  *rrr=*(rrr+1)=*(rrr+2)=0;
+  rrr[0]=rrr[1]=rrr[2]=0;
   cc=0;
   if(x1>x2){a=x1; x1=x2; x2=a; a=y1; y1=y2; y2=a;}
   a=(y1-y2)/(x1-x2);
@@ -66,27 +64,27 @@ char *ave1(unsigned char *F,double x1,double y1,double x2,double y2,int v){
   dx=(x2-x1)/len/2;
   for(x=x1;x<=x2;x+=dx){
     y=a*x+b;
-    rr=read1(F,x,y);
-    *rrr+=*rr; *(rrr+1)+=*(rr+1); *(rrr+2)+=*(rr+2); cc++;
+    read1(F,rr,x,y);
+    rrr[0]+=rr[0]; rrr[1]+=rr[1]; rrr[2]+=rr[2]; cc++;
     if(v==1){
-      rr=read1(F,x+1,y);
-      *rrr+=*rr; *(rrr+1)+=*(rr+1); *(rrr+2)+=*(rr+2); cc++;
-      rr=read1(F,x-1,y);
-      *rrr+=*rr; *(rrr+1)+=*(rr+1); *(rrr+2)+=*(rr+2); cc++;
-      rr=read1(F,x,y+1);
-      *rrr+=*rr; *(rrr+1)+=*(rr+1); *(rrr+2)+=*(rr+2); cc++;
-      rr=read1(F,x,y-1);
-      *rrr+=*rr; *(rrr+1)+=*(rr+1); *(rrr+2)+=*(rr+2); cc++;
+      read1(F,rr,x+1,y);
+      rrr[0]+=rr[0]; rrr[1]+=rr[1]; rrr[2]+=rr[2]; cc++;
+      read1(F,rr,x-1,y);
+      rrr[0]+=rr[0]; rrr[1]+=rr[1]; rrr[2]+=rr[2]; cc++;
+      read1(F,rr,x,y+1);
+      rrr[0]+=rr[0]; rrr[1]+=rr[1]; rrr[2]+=rr[2]; cc++;
+      read1(F,rr,x,y-1);
+      rrr[0]+=rr[0]; rrr[1]+=rr[1]; rrr[2]+=rr[2]; cc++;
     }
   }
-  *ooo=(unsigned char)(*rrr/cc);
-  *(ooo+1)=(unsigned char)(*(rrr+1)/cc);
-  *(ooo+2)=(unsigned char)(*(rrr+2)/cc);
-  return ooo;
+  ooo[0]=(unsigned char)(rrr[0]/cc);
+  ooo[1]=(unsigned char)(rrr[1]/cc);
+  ooo[2]=(unsigned char)(rrr[2]/cc);
+  return;
 }
 
 int main(int argc,char **argv){
-  unsigned char F[32784],*ooo;
+  unsigned char F[32784],ooo[3];
   FILE *fp;
   time_t now;
   struct tm *now_tm;
@@ -107,10 +105,10 @@ int main(int argc,char **argv){
   hr=(90.0-30.0*(hh+mm/60))*2.0*M_PI/360.0;
   mr=(90.0-6.0*mm)*2.0*M_PI/360.0;
 
-  ooo=ave1(F,31,31,31+18*cos(hr),63-31-18*sin(hr),1);
-  line1(F,31,31,31+18*cos(hr),63-31-18*sin(hr),1,ooo);
-  ooo=ave1(F,31,31,31+28*cos(mr),63-31-28*sin(mr),2);
-  line1(F,31,31,31+28*cos(mr),63-31-28*sin(mr),2,ooo);
+  ave1(F,ooo,31,31,31+18*cos(hr),63-31-18*sin(hr),1);
+  line1(F,ooo,31,31,31+18*cos(hr),63-31-18*sin(hr),1);
+  ave1(F,ooo,31,31,31+28*cos(mr),63-31-28*sin(mr),2);
+  line1(F,ooo,31,31,31+28*cos(mr),63-31-28*sin(mr),2);
 
   // write ff file
   fp=fopen(argv[2],"wb");

a79988faa28d2c00b9285a142e40a577d2478041
Gianluca Mazzini <gianluca@mazzini.org>
2023-10-31 06:42:01 +0100

    Update clock.c

--- clock.c
+++ clock.c
@@ -57,6 +57,7 @@ int main(int argc,char **argv){
   mm=now_tm->tm_min;
   if(hh>11)hh-=12;
   hr=(90-30*hh)*2*M_PI/360;
+  mm=45;
   mr=(90-6*mm)*2*M_PI/360;
   line1(F,31,31,(int)floor(31+20*cos(hr)),(int)floor(31+20+sin(hr)),30,0,0);
   line1(F,31,31,(int)floor(31+30*cos(mr)),(int)floor(31+30+sin(mr)),0,30,0);

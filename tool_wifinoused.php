2308fef94d6d9197719ff9cd8afbc36b2278bbd1
Gianluca Mazzini <gianluca@mazzini.org>
2023-11-02 11:12:24 +0100

    Update tool_wifinoused.php

--- tool_wifinoused.php
+++ tool_wifinoused.php
@@ -5,13 +5,19 @@ $conn=oci_connect($p1,$p2,$p3);
 $te=(int)(time()/86400);
 $ts=$te-7;
 
-$query=oci_parse($conn,"select idstart,idend from idistat");
+$query=oci_parse($conn,"select idstart,idend,eistat from idistat");
 oci_execute($query);
 for($cc=0;;$cc++){
   $row=oci_fetch_row($query);
   if($row==null)break;
   $idstart[$cc]=(int)$row[0];
   $idend[$cc]=(int)$row[1];
+  $eistat[$cc]=$row[2];
+  $query1=oci_parse($conn,"select ente from istatente where istat='$eistat'");
+  oci_execute($query1);
+  $row1=oci_fetch_row($query1);
+  $ente[$c]=$row1[0];
+  oci_free_statement($query1);
 }
 oci_free_statement($query);
 
@@ -21,7 +27,7 @@ for($c=0;$c<$cc;$c++){
   $row=oci_fetch_row($query);
   $enn=(int)$row[0];
   oci_free_statement($query);
-  if($enn==0)echo "$idstart[$c],$idend[$c]\n";
+  if($enn==0)echo "$idstart[$c],$idend[$c],$eistat[$c],$ente[$c]\n";
 }
 
 oci_close($conn);

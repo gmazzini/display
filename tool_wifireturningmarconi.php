dcb43d660014085e62fa7694331cbc6be56028a2
Gianluca Mazzini <gianluca@mazzini.org>
2023-11-14 05:40:13 +0100

    Create tool_wifireturningmarconi.php

---
+++ tool_wifireturningmarconi.php
@@ -0,0 +1,33 @@
+<?php
+
+include "data.php";
+$conn=oci_connect($p1,$p2,$p3);
+$tts=$argv[1];
+$tte=$argv[2];
+
+$query=oci_parse($conn,"select distinct id from dhcpwifi where (ip>=14016 and ip<=14047) and tt=$tts");
+oci_execute($query);
+for($i=0;;$i++){
+  $row=oci_fetch_row($query);
+  if($row==null)break;
+  $id[$i]=$row[0];
+}
+oci_free_statement($query);
+echo "user: $i\n";
+
+for($tt=$tts+1;$tt<=$tte;$tt++){
+  $ret[$tt]=0;
+  for($j=0;$j<$i;$j++){
+    $query=oci_parse($conn,"select count(distint id) from dhcpwifi where id='$id[$j]' and tt=$tt and (ip>=14016 and ip<=14047)");
+    oci_execute($query);
+    $row=oci_fetch_row($query);
+    if($row[0]>0)$ret[$tt]++;
+    oci_free_statement($query);
+  }
+}
+
+for($tt=$tts+1;$tt<=$tte;$tt++)echo "$tt,$ret[$tt]\n";
+
+oci_close($conn);
+
+?>

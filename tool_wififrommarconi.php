00ec379985a98a8795d035938c4162c7e0580074
Gianluca Mazzini <gianluca@mazzini.org>
2023-11-15 07:22:01 +0100

    Update tool_wififrommarconi.php

--- tool_wififrommarconi.php
+++ tool_wififrommarconi.php
@@ -21,7 +21,10 @@ foreach($id as $k => $v){
   $query=oci_parse($conn,"select count(distinct id) from dhcpwifi where id='$k' and (tt>=$tts and tt<=$tte) and (ip>=14016 and ip<=14047)");
   oci_execute($query);
   $row=oci_fetch_row($query);
-  if($row[0]>($tte-$tts)*$thr)unset($id[$k]);
+  if($row[0]>($tte-$tts)*$thr){
+    echo "$k\n";
+    unset($id[$k]);
+  }
   oci_free_statement($query);
 }
 $tot2=count($id);

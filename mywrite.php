aaf2207d63bfece1217e055ee7f3d021c9427b3e
Gianluca Mazzini <gianluca@mazzini.org>
2023-11-29 06:31:12 +0100

    Update mywrite.php

--- mywrite.php
+++ mywrite.php
@@ -1,15 +1,15 @@
 <?php
 
+$base="/home/www/www.chaos.cc/dev";
 $mydes=$_GET["des"];
-$fp=fopen("/home/www/www.chaos.cc/qq.des","w");
+$fp=fopen("$base/qq.des","w");
 $mydes=str_replace("\\","\n",$mydes);
 fprintf($fp,"%s",$mydes);
 fclose($fp);
-shell_exec("/home/www/www.chaos.cc/write2 /home/www/www.chaos.cc/qq.des 4 /home/www/www.chaos.cc/qq.ff; /home/w
-ww/www.chaos.cc/convert3 /home/www/www.chaos.cc/qq.ff 1 /home/www/www.chaos.cc/qq.bin");
-$len=filesize("/home/www/www.chaos.cc/qq.bin");
+shell_exec("$base/write2 $base/qq.des 4 $base/qq.ff; $base/convert3 $base/qq.ff 1 $base/qq.bin");
+$len=filesize("$base/qq.bin");
 header("Content-Type: application/octet-stream");
 header("Content-Length: $len");
-readfile("/home/www/www.chaos.cc/qq.bin");
+readfile("$base/qq.bin");
 
 ?>

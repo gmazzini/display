77f28b80a7322f221eef13a0e36dbd4d82644a0d
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-21 07:04:14 +0100

    Update tool_wifirer.php

--- tool_wifirer.php
+++ tool_wifirer.php
@@ -26,36 +26,5 @@ for($c=0;$c<$cc;$c++){
   printf("%d,%d,%d\n",$tt,$c,$nn[$c]);
 }
 
-exit(0);
-
-$query=oci_parse($conn,"select idstart,idend from idistat where eistat='$istat'");
-oci_execute($query);
-for($cc=0;;$cc++){
-  $row=oci_fetch_row($query);
-  if($row==null)break;
-  $idstart[$cc]=(int)$row[0];
-  $idend[$cc]=(int)$row[1];
-}
-oci_free_statement($query);
-
-for($tt=$ts;$tt<$te;$tt++){
-  $query=oci_parse($conn,"select count(distinct id) from dhcpwifi where istat='$istat' and tt=$tt");
-  oci_execute($query);
-  $row=oci_fetch_row($query);
-  $nn=(int)$row[0];
-  oci_free_statement($query);
-
-  $enn=0;
-  for($c=0;$c<$cc;$c++){
-    $query=oci_parse($conn,"select count(distinct id) from dhcpwifi where tt=$tt and ip>=$idstart[$c] and ip<=$idend[$c]");
-    oci_execute($query);
-    $row=oci_fetch_row($query);
-    $enn+=(int)$row[0];
-    oci_free_statement($query);
-  }
-
-  echo "$tt,$nn,$enn\n";
-}
-
 oci_close($conn);
 ?>

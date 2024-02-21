d497ae0bd77c6c8bab8a8aec65935a8155e7064d
Gianluca Mazzini <gianluca@mazzini.org>
2024-02-21 07:21:33 +0100

    Update tool_wifirer.php

--- tool_wifirer.php
+++ tool_wifirer.php
@@ -4,7 +4,7 @@ include "data.php";
 $conn=oci_connect($p1,$p2,$p3);
 $tt=(int)$argv[1];
 $lab=array("11271"=>"Presidente","11452"=>"AM38","11456"=>"AM5052","11460"=>"AM18","11464"=>"AM21","11468"=>"AM30","11472"=>"AM44","11476"=>"AM64","11480"=>"Fiera8",
-          "11484"=>"Silvani","11500"="Galliera","11502"=>"Marsala","14438"=>"StacpPR","15112"=>"POP","15231"=>"ImolaARL","15296"=>"Campus");
+          "11484"=>"Silvani","11500"=>"Galliera","11502"=>"Marsala","14438"=>"StacpPR","15112"=>"POP","15231"=>"ImolaARL","15296"=>"Campus");
 
 $query=oci_parse($conn,"select idstart,idend from idistat where eistat='00008'");
 oci_execute($query);

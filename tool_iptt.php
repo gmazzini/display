0bea1b72eab00d4a25d961339985d6a808c4e388
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-14 10:36:31 +0100

    Update tool_iptt.php

--- tool_iptt.php
+++ tool_iptt.php
@@ -12,22 +12,13 @@ $min=$row[0];
 $max=$row[1];
 oci_free_statement($query0);
 
-echo "$min,$max\n";
-exit(1);
-
-
-for(;;){
-  $row=oci_fetch_row($query0);
-  if($row==null)break;
-  $tt=$row[0];
-  
+for($tt=min;$tt<=$max;$tt++){
   $query=oci_parse($conn,"select count(distinct id) from dhcpwifi where tt=$tt and ip>=$ips and ip<=$ipe");
   oci_execute($query);
   $row=oci_fetch_row($query);
   echo "$tt,$row[0]\n";
   oci_free_statement($query);
 }
-oci_free_statement($query0);
 
 oci_close($conn);
 

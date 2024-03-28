3821b937e0775696e2087e9d1f23e6de06503cc5
Gianluca Mazzini <gianluca@mazzini.org>
2024-03-28 22:49:30 +0100

    Update myrefresh.php

--- myrefresh.php
+++ myrefresh.php
@@ -67,9 +67,6 @@ oci_free_statement($query);
 make3($conn,"apwifi","apwifi","1cgCtacbWsm7wybTp8cA7wWBFo9bZOSc7JAnlV99K-O0","WISPER!H2:I",0,1,$sovra,$ss);
 make3($conn,"uiftth","uiftth","1Nk39CPjf9Lu_UQ_zUnY97cqYZ5Vh7K00owrw-XeSgHM","B4:D",0,2,$sovra,$ss);
 
-
-exit(1);
-
 $query=oci_parse($conn,"select distinct istat from idistat where istat>'30000'");
 oci_execute($query);
 for($yy=0;;){

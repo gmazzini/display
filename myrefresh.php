04169288d0acfd574cb756b1147a10c06d575b1b
Gianluca Mazzini <gianluca@mazzini.org>
2024-04-02 13:31:39 +0200

    Update myrefresh.php

--- myrefresh.php
+++ myrefresh.php
@@ -23,6 +23,9 @@ for($ss=0;;){
 }
 oci_free_statement($query);
 
+make4($conn,"uiftth","uiftth","11EyZCYlMyiGn9GetrFq1WpOFDcHEBegSBOkzaJYmAsA","Comuni!B3:E",0,3,"",$sovra,$ss);
+exit(0);
+
 make5($conn,"attivifse","attivi","https://dati.fascicolo-sanitario.it/rest/attivi/comune",$sovra,$ss);
 make5($conn,"accessifse","accessi","https://dati.fascicolo-sanitario.it/rest/accessi/comune",$sovra,$ss);
 make5($conn,"scaricatifse","scaricati","https://dati.fascicolo-sanitario.it/rest/documenti/comune",$sovra,$ss);
@@ -36,23 +39,6 @@ make4($conn,"apwifi","apwifi","1ZMxZbD-gGhLGR5RdcBHhRsZQeARiRrK1Nw-2PSWHbZE","Ap
 make4($conn,"scuole","scuole","1SUGh7fL0zkppRbYZykrdVoim5XLKv9Vwt1yDstLPu-o","Elenco!N2:U",0,-1,"@\$oo['values'][\$i][7]=='BULBUL'",$sovra,$ss);
 make4($conn,"man","man","1DEs7yoAfJ6wK9L-V5kYoArEeP-g110NgPMU0DDFv9EE","MAN!K2:V",11,-1,"\$oo['values'][\$i][0]=='MAN'",$sovra,$ss);
 make4($conn,"pal","pal","1DEs7yoAfJ6wK9L-V5kYoArEeP-g110NgPMU0DDFv9EE","PAL!V2:V",0,-1,"1==1",$sovra,$ss);
-
-make3($conn,"uiftth","uiftth","1Nk39CPjf9Lu_UQ_zUnY97cqYZ5Vh7K00owrw-XeSgHM","B4:D",0,2,"",$sovra,$ss);
-
-// fai2($conn,1,"man","man","https://docs.google.com/spreadsheets/d/1DEs7yoAfJ6wK9L-V5kYoArEeP-g110NgPMU0DDFv9EE/gviz/tq?tq=select%20V%20where%20K%3D%27MAN%27&tqx=out:csv&gid=902689105",$sovra,$ss);
-// fai2($conn,1,"pal","pal","https://docs.google.com/spreadsheets/d/1DEs7yoAfJ6wK9L-V5kYoArEeP-g110NgPMU0DDFv9EE/gviz/tq?tq=select%20V&tqx=out:csv&gid=1797209276",$sovra,$ss);
-// fai2($conn,1,"scuole","scuole","https://docs.google.com/spreadsheets/d/10xN81W5Dd8LRjVOm_FJ0ubzgJ1EWJ4Vfi8P3iVZdBho/gviz/tq?tq=select%20A%20where%20U%3D%27BULBUL%27&tqx=out:csv&gid=566741345",$sovra,$ss);
-// fai2($conn,1,"areeaai","areeaai","https://docs.google.com/spreadsheets/d/1cgCtacbWsm7wybTp8cA7wWBFo9bZOSc7JAnlV99K-O0/gviz/tq?tq=select%20G&tqx=out:csv&gid=566741345",$sovra,$ss);
-// fai2($conn,3,"uiftth","uiftth","https://docs.google.com/spreadsheets/d/1Nk39CPjf9Lu_UQ_zUnY97cqYZ5Vh7K00owrw-XeSgHM/gviz/tq?tq=select%20B%2CD&tqx=out:csv",$sovra,$ss);
-// fai2($conn,1,"aziendeaai","aziendeaai","https://docs.google.com/spreadsheets/d/1cgCtacbWsm7wybTp8cA7wWBFo9bZOSc7JAnlV99K-O0/gviz/tq?tq=select%20G%2CF&tqx=out:csv&gid=566741345",$sovra,$ss);
-// fai2($conn,1,"apwifi","apwifi","https://docs.google.com/spreadsheets/d/1cgCtacbWsm7wybTp8cA7wWBFo9bZOSc7JAnlV99K-O0/gviz/tq?tq=select%20H%2CI&tqx=out:csv&gid=1373772362",$sovra,$ss);
-
-// fai1($conn,"attivifse","attivi","https://dati.fascicolo-sanitario.it/rest/attivi/comune",$sovra,$ss);
-// fai1($conn,"accessifse","accessi","https://dati.fascicolo-sanitario.it/rest/accessi/comune",$sovra,$ss);
-// fai1($conn,"scaricatifse","scaricati","https://dati.fascicolo-sanitario.it/rest/documenti/comune",$sovra,$ss);
-// fai1($conn,"attivazionilepidaid","attivazioni","https://dati.fascicolo-sanitario.it/rest/lepidaid/attivazioni/comune",$sovra,$ss);
-// fai1($conn,"accessilepidaid","accessi","https://dati.fascicolo-sanitario.it/rest/lepidaid/accessi/comune",$sovra,$ss);
-// fai1($conn,"sportellilepidaid","sportelli","https://dati.fascicolo-sanitario.it/rest/lepidaid/sportelli/comune",$sovra,$ss);
 
 $query=oci_parse($conn,"select distinct istat from idistat where istat>'30000'");
 oci_execute($query);
@@ -111,85 +97,6 @@ for($i=0;$i<$ss;$i++){
   $query=oci_parse($conn,"insert into $table (istat,$field,tt) values ('$kk',$vv,$tt)");
   oci_execute($query);
   oci_free_statement($query);
-}
-
-function fai1($conn,$table,$field,$url,$sovra,$ss){
-  echo "$table\n";
-  $aux=json_decode(file_get_contents("$url"),true);
-  foreach($aux["dati"] as $k => $v){
-    $kk=substr($k,1,5);
-    $vv=(int)$v["$field"];
-    if(mycheck($conn,$table,$kk))$query=oci_parse($conn,"update $table set $field=$vv where istat='$kk'");
-    else $query=oci_parse($conn,"insert into $table (istat,$field) values ('$kk',$vv)");
-    oci_execute($query);
-    oci_free_statement($query);
-  }
-  $query=oci_parse($conn,"select sum($field) from $table where istat>'30000'");
-  oci_execute($query);
-  $row=oci_fetch_row($query);
-  $vv=$row[0];
-  oci_free_statement($query);
-  $kk="00008";
-  if(mycheck($conn,$table,$kk))$query=oci_parse($conn,"update $table set $field=$vv where istat='$kk'");
-  else $query=oci_parse($conn,"insert into $table (istat,$field) values ('$kk',$vv)");
-  oci_execute($query);
-  oci_free_statement($query);
-  for($i=0;$i<$ss;$i++){
-    $query=oci_parse($conn,"select sum($table.$field) from $table,idistat where $table.istat=idistat.istat and idistat.sovra='$sovra[$i]'");
-    oci_execute($query);
-    $row=oci_fetch_row($query);
-    if(isset($row[0]))$vv=$row[0];
-    else $vv=0;
-    oci_free_statement($query);
-    $kk=$sovra[$i];
-    if(mycheck($conn,$table,$kk))$query=oci_parse($conn,"update $table set $field=$vv where istat='$kk'");
-    else $query=oci_parse($conn,"insert into $table (istat,$field) values ('$kk',$vv)");
-    oci_execute($query);
-    oci_free_statement($query);
-  }
-}
-
-function fai2($conn,$base,$table,$field,$url,$sovra,$ss){
-  echo "$table\n";
-  $aux=explode("\n",file_get_contents("$url"));
-  for($i=$base;;$i++){
-    if(!isset($aux[$i]))break;
-    $aa=explode(",",$aux[$i]);
-    if(strlen($aa[0])<5)continue;
-    $kk=substr($aa[0],1,5);
-    if(!is_numeric($kk))continue;
-    if(isset($aa[1]))$vv=substr($aa[1],1,strlen($aa[1])-2);
-    else $vv=1;
-    @$ddd[$kk]+=(int)$vv;
-  }
-  foreach($ddd as $kk => $vv){
-    if(mycheck($conn,$table,$kk))$query=oci_parse($conn,"update $table set $field=$vv where istat='$kk'");
-    else $query=oci_parse($conn,"insert into $table (istat,$field) values ('$kk',$vv)");
-    oci_execute($query);
-  }
-  $query=oci_parse($conn,"select sum($field) from $table where istat>'30000'");
-  oci_execute($query);
-  $row=oci_fetch_row($query);
-  $vv=$row[0];
-  oci_free_statement($query);
-  $kk="00008";
-  if(mycheck($conn,$table,$kk))$query=oci_parse($conn,"update $table set $field=$vv where istat='$kk'");
-  else $query=oci_parse($conn,"insert into $table (istat,$field) values ('$kk',$vv)");
-  oci_execute($query);
-  oci_free_statement($query);
-  for($i=0;$i<$ss;$i++){
-    $query=oci_parse($conn,"select sum($table.$field) from $table,idistat where $table.istat=idistat.istat and idistat.sovra='$sovra[$i]'");
-    oci_execute($query);
-    $row=oci_fetch_row($query);
-    if(isset($row[0]))$vv=$row[0];
-    else $vv=0;
-    oci_free_statement($query);
-    $kk=$sovra[$i];
-    if(mycheck($conn,$table,$kk))$query=oci_parse($conn,"update $table set $field=$vv where istat='$kk'");
-    else $query=oci_parse($conn,"insert into $table (istat,$field) values ('$kk',$vv)");
-    oci_execute($query);
-    oci_free_statement($query);
-  }
 }
 
 oci_close($conn);

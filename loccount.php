45beebd278a59d90b54ce8d1b674ed823487c597
Gianluca Mazzini <gianluca@mazzini.org>
2024-10-02 07:08:38 +0200

    Update loccount.php

--- loccount.php
+++ loccount.php
@@ -1,24 +1,16 @@
 <?php
 
 include "data.php";
+date_default_timezone_set("UTC");
 $conn=oci_connect($p1,$p2,$p3);
-
-$query=oci_parse($conn,"select count(*) from dhcpwifi");
-oci_execute($query);
-$row=oci_fetch_row($query);
-$v2=$row[0];
-oci_free_statement($query);
 $query=oci_parse($conn,"select count(distinct fnv1a) from dhcpwifi");
 oci_execute($query);
 $row=oci_fetch_row($query);
-$v3=$row[0];
+$qq=$row[0];
 oci_free_statement($query);
-
-$tt=(int)(time()/86400);
-oci_close($conn);
-
-$vv2=$v2.",".$v3;
-$rr=$tt-19995;
+$dt=(int)(time()/86400)-19998+2;
+$dd=date("G")+3;
+$col=($dd<26)?chr(65+$dd):chr((int)($dd/26)+64).chr($dd%26+65);
 include "/home/www/restdati.lepida.it/googleset.php";
 $access_token=file_get_contents("/home/www/data/access_token");
 $curlPost='
@@ -26,10 +18,10 @@ $curlPost='
   "valueInputOption": "RAW",
   "data": [
     {
-      "range": "users!A'.$rr.':C'.$rr.'",
+      "range": "data!'.$col.$dt.'",
       "majorDimension": "ROWS",
       "values": [
-        ['.$tt.','.$vv2.']
+        ['.$qq.']
       ]
     }
   ]

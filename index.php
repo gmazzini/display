909be074de9d334d87187cfbdc8c6762a5c84914
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-13 16:32:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 $ip=$_GET["ip"];
 @$ser=$_GET["ser"];
 if($ser=="")$ser="0000";
-$des="(/home/www/display/files/$ip.des";
+$des="/home/www/display/files/$ip.des";
 $ff="/home/www/display/files/$ip.ff";
 $bin="/home/www/display/files/$ip.bin";
 
 $fp=fopen($des,"w");
-fprintf($fp,"1 -2 0 FF0000FF 00000000 3 %sZ\n",date("d.m.y H:i"));
+fprintf($fp,"1 -2 0 FF0000FF 00000000 3 %sZ\n",date("d.m.y H:i:s"));
 fprintf($fp,"1 -2 6 FFFFFFFF 00000000 3 %s %s\n",$ip,$ser);
-shell_exec("tmp/write2 $des $ff; tmp/convert3 $ff 5 $bin");
+fprintf($fp,"1 -2 12 00FF00FF 00000000 3 Ciao a tutti\n");
+shell_exec("/home/www/display/write2 $des $ff; /home/www/display/convert3 $ff 8 $bin");
 
 $len=filesize($bin);
 header("Content-Type: application/octet-stream");

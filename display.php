bf693e4727cd70521530d4e6097de03e94af5edc
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-16 11:34:56 +0100

    Refactor display.php for improved file handling

--- display.php
+++ display.php
@@ -2,28 +2,24 @@
 
 $ip=$_GET["ip"];
 $ser=$_GET["ser"];
-$bin="/home/www/display/files/$ip.bin";
+@mkdir("/run/display",0777,true);
+$bin="/run/display/$ser.bin";
+$n=(int)@file_get_contents("/run/display/$ser.step")+1;
+file_put_contents("/run/display/$ser.step",(string)$n,LOCK_EX);
+
 $files=glob('/home/www/display/images/[0-9][0-9][0-9][0-9].ff');
 shuffle($files);
 $ff=$files[0];
+
 shell_exec("/home/www/display/convert3 $ff 40 $bin");
 
 @ini_set('zlib.output_compression', '0');
-@apache_setenv('no-gzip', '1');
+@apache_setenv('no-gzip','1');
 header('Content-Encoding: identity');
 while (ob_get_level()) { ob_end_clean(); }
 $len=filesize($bin);
 header("Content-Type: application/octet-stream");
 header("Content-Length: $len");
 readfile($bin);
-
-
-function next_display($ser){
-  @mkdir("/run/display",0777,true);
-  $f="/run/display/$ser";
-  $n=(int)@file_get_contents($f)+1;
-  file_put_contents($f,(string)$n,LOCK_EX);
-  return $n;
-}
 
 ?>

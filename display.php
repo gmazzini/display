d35bd3fd9a7f01366373d90ead4d3fccb969be93
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-16 14:37:27 +0100

    Comment out $ff assignment from shuffled files
    Comment out the assignment of the variable $ff from the shuffled files array.

--- display.php
+++ display.php
@@ -5,12 +5,13 @@ $ser=$_GET["ser"];
 @mkdir("/run/display",0777,true);
 $bin="/run/display/$ser.bin";
 $des="/run/display/$ser.des";
+$ff="/run/display/$ser.ff";
 $n=(int)@file_get_contents("/run/display/$ser.step")+1;
 file_put_contents("/run/display/$ser.step",(string)$n."\n",LOCK_EX);
 
 $files=glob("/home/www/display/images/[0-9][0-9][0-9][0-9].ff");
 shuffle($files);
-$ff=$files[0];
+// $ff=$files[0];
 
 file_put_contents($des,"#\n1 0 0 FF0000FF 00000000 4 $n\n");
 

3fc7ef937109af099af9fc5229acb79b447b1d42
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-16 14:35:46 +0100

    Enhance display.php with descriptor file handling
    Add functionality to create and write to descriptor files.

--- display.php
+++ display.php
@@ -4,13 +4,17 @@ $ip=$_GET["ip"];
 $ser=$_GET["ser"];
 @mkdir("/run/display",0777,true);
 $bin="/run/display/$ser.bin";
+$des="/run/display/$ser.des";
 $n=(int)@file_get_contents("/run/display/$ser.step")+1;
 file_put_contents("/run/display/$ser.step",(string)$n."\n",LOCK_EX);
 
 $files=glob("/home/www/display/images/[0-9][0-9][0-9][0-9].ff");
 shuffle($files);
 $ff=$files[0];
-shell_exec("/home/www/display/convert3 $ff 40 $bin");
+
+file_put_contents($des,"#\n1 0 0 FF0000FF 00000000 4 $n\n");
+
+shell_exec("/home/www/display/write2 $des $ff; /home/www/display/convert3 $ff 40 $bin");
 
 @ini_set("zlib.output_compression","0");
 @apache_setenv("no-gzip","1");

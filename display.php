7a208ca9d51fcf0deb6a2a66e543802e304fb9dd
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-16 09:42:42 +0100

    Add display.php for file serving and conversion

---
+++ display.php
@@ -0,0 +1,20 @@
+<?php
+
+$ip=$_GET["ip"];
+$ser=$_GET["ser"];
+$bin="/home/www/display/files/$ip.bin";
+$files=glob('/home/www/display/images/[0-9][0-9][0-9][0-9].ff');
+shuffle($files);
+$ff=$files[0];
+shell_exec("/home/www/display/convert3 $ff 40 $bin");
+
+@ini_set('zlib.output_compression', '0');
+@apache_setenv('no-gzip', '1');
+header('Content-Encoding: identity');
+while (ob_get_level()) { ob_end_clean(); }
+$len=filesize($bin);
+header("Content-Type: application/octet-stream");
+header("Content-Length: $len");
+readfile($bin);
+
+?>

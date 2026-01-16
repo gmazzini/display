141d6a9290d46ccc56da36d6f1abb1b6bd172eb9
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-16 11:55:15 +0100

    Refactor file handling and output compression settings

--- display.php
+++ display.php
@@ -7,15 +7,14 @@ $bin="/run/display/$ser.bin";
 $n=(int)@file_get_contents("/run/display/$ser.step")+1;
 file_put_contents("/run/display/$ser.step",(string)$n."\n",LOCK_EX);
 
-$files=glob('/home/www/display/images/[0-9][0-9][0-9][0-9].ff');
+$files=glob("/home/www/display/images/[0-9][0-9][0-9][0-9].ff");
 shuffle($files);
 $ff=$files[0];
-
 shell_exec("/home/www/display/convert3 $ff 40 $bin");
 
-@ini_set('zlib.output_compression', '0');
-@apache_setenv('no-gzip','1');
-header('Content-Encoding: identity');
+@ini_set("zlib.output_compression","0");
+@apache_setenv("no-gzip","1");
+header("Content-Encoding: identity");
 while (ob_get_level()) { ob_end_clean(); }
 $len=filesize($bin);
 header("Content-Type: application/octet-stream");

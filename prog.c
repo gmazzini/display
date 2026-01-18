5e2785f25f4c2fb58c61106f0d99830d68ce038d
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-18 05:14:14 +0100

    Create prog.c

---
+++ prog.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+// v[0]=n v[1]=hh v[2]=mm v[3]=ss
+void main(int argc,char *argv[]){
+  FILE *fp,*fp1;
+  char buf[100],*p1,*p2,*p3,*q1,*q2,*q3,*q4,*x;
+  int l,tot,ln,go,min,max,i;
+  long v[100];
+  time_t now;
+  struct tm tmv;
+
+  now=time(NULL);
+  localtime_r(&now,&tmv);
+  v[1]=tmv.tm_hour;
+  v[2]=tmv.tm_min;
+  v[3]=tmv.tm_sec;
+
+  sprintf(buf,"/run/display/%s.step",argv[1]);
+  fp=fopen(buf,"rt");
+  fgets(buf,100,fp);
+  v[0]=atol(buf);
+  fclose(fp);
+  srand((unsigned)time(NULL));
+
+  sprintf(buf,"/run/display/%s.des",argv[1]);
+  fp1=fopen(buf,"wt");
+
+  fp=fopen("/home/www/display/pgr/001.seq","rt");
+  fgets(buf,100,fp);
+  tot=atoi(buf);
+  ln=v[0]%tot;
+  go=0;
+  for(;;){
+    fgets(buf,100,fp);
+    if(feof(fp))break;
+    l=strlen(buf); buf[l-1]='\0';
+
+    if(go==0 && buf[0]=='['){
+      p1=strtok(buf+1," ");
+      p2=strtok(NULL," ");
+      p3=strtok(NULL," ]");
+      if(ln>=atoi(p1) && ln<=atoi(p2))go=1;
+      continue;
+    }
+
+    for(x=buf;*x!='\0';x++){
+      if(*x=='@'){
+        q1=x+1; for(x=q1;*x!='$';x++); *x='\0';
+        q2=x+1; for(x=q2;*x!='$';x++); *x='\0';
+        fprintf(fp1,q1,v[atoi(q2)]);
+        continue;
+      }
+      fprintf(fp1,"%c",*x);
+    }
+    fprintf(fp1,"\n");
+
+  }
+  fclose(fp);
+  fclose(fp1);
+}
+root@s018:/hom
+    if(go==1 && *buf=='[')break;
+
+    if(go==0)continue;
+
+    if(buf[0]=='@'){
+      q1=strtok(buf+1," ");
+      q2=strtok(NULL," ");
+      if(strcmp(q2,"RAND")==0){
+        q3=strtok(NULL," "); min=atoi(q3);
+        q4=strtok(NULL," \n"); max=atoi(q4);
+        v[atoi(q1)]=min+rand()%(max-min+1);
+      }
+      continue;
+    }

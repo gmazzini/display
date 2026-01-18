21c3b2e6e8499804917cb02b560615fcd14c7499
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-18 05:08:44 +0100

    Update giulia.php

--- giulia.php
+++ giulia.php
@@ -1,12 +1,11 @@
 <?php
 
 $base="/run/display/giulia";
-$des=$_GET["des"];
 $fp=fopen("$base.des","w");
-$des=str_replace("\\","\n",$des);
+$des=str_replace("\\","\n",$_GET["des"]);
 fprintf($fp,"%s",$des);
 fclose($fp);
-shell_exec("/home/www/display/write4 $base.des $base.ff $base.bin");
+shell_exec("/home/www/display/write3 $base.des $base.ff $base.bin");
 $len=filesize("$base.bin");
 header("Content-Type: application/octet-stream");
 header("Content-Length: $len");

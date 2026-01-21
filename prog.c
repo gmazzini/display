6f631270c9779c57050fdbd1266868685c8cf72d
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-21 22:33:26 +0100

    Update prog.c

--- prog.c
+++ prog.c
@@ -84,7 +84,7 @@ void main(int argc,char *argv[]){
         a4=atoi(buf);
         fclose(fp2);
 //        sprintf(v[a0],fmt,a3+a4);
-        sprintf(v[a0],"%d",10);
+        sprintf(v[a0],"%d",a3);
       }
       
       continue;

34d5d466dd6d79112fb736e9403f28b3349c281a
Gianluca Mazzini <gianluca@mazzini.org>
2026-01-23 17:02:12 +0100

    Update sched.c

--- sched.c
+++ sched.c
@@ -70,19 +70,26 @@ void *client(void *p){
       }
       if(go==1 && seq[r][0]=='[')break;
       if(go==0)continue;
-      
       if(seq[r][0]=='@'){
         q=strtok(seq[r]+1," "); a0=atoi(q);
         q=strtok(NULL," ");
+        if(strcmp(q,"RAND")==0){
+          q=strtok(NULL," "); strcpy(fmt,q);
+          q=strtok(NULL," "); a1=atoi(q);
+          q=strtok(NULL," "); a2=atoi(q);
+          sprintf(v[a0],fmt,a1+rand()%(a2-a1+1));
+        }
         
         continue;
       }
       for(x=seq[r];*x!='\0';x++){
+        /*
         if(*x=='@'){
           q1=x+1; for(x=q1;*x!='$';x++); *x='\0';
           fprintf(fp,"%s",v[atoi(q1)]);
           continue;
         }
+        */
         fprintf(fp,"%c",*x);
       }
       fprintf(fp,"\n");
